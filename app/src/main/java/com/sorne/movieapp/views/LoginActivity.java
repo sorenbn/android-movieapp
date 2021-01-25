becd1c5ced4f412b6f1a4acd1772f343f0e74775
sorne bachse <sorenbn@outlook.com>
2021-01-25 14:14:44 +0100

    added design for login activity

---
+++ app/src/main/java/com/sorne/movieapp/views/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.sorne.movieapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sorne.movieapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
